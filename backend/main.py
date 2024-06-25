9868cc552226ea3e6d926cb01aea6aa2ca9a8866
naandan <nandanramdani608@gmail.com>
2024-06-25 23:38:36 +0700

    add: custom-path

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, Namespace
 import random
 import time
 from threading import Thread
@@ -8,15 +8,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+class CustomNamespace(Namespace):
+    def on_connect(self):
+        print('Client connected to custom namespace')
+
+    def on_disconnect(self):
+        print('Client disconnected from custom namespace')
+
+# Daftarkan namespace pada path /custom-path
+socketio.on_namespace(CustomNamespace('/custom-path'))
+
 def send_random_data():
     while True:
         time.sleep(5)  # Mengirim data setiap 5 detik
         data = {'value': random.random()}
-        socketio.emit('updateForm', data)
+        socketio.emit('updateForm', data, namespace='/custom-path')
 
 @app.route('/')
 def index():
-    return "WebSocket Server is running"
+    return "WebSocket Server with Custom Path is running"
 
 if __name__ == '__main__':
     thread = Thread(target=send_random_data)
