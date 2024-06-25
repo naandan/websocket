01ed9532685370ea6b403fc09edcfbcc7cb5a008
naandan <nandanramdani608@gmail.com>
2024-06-26 00:29:53 +0700

    fix

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,18 @@ import random
 
 async def send_random_data(websocket, path):
     print(f"Client connected to {path}")
+    if path != "/custom-path/":
+        return
     try:
         while True:
             data = {'value': random.random()}
             await websocket.send(json.dumps(data))
-            await asyncio.sleep(5)  # Mengirim data setiap 5 detik
+            await asyncio.sleep(5)
     except websockets.ConnectionClosed:
         print("Client disconnected")
 
-start_server = websockets.serve(send_random_data, "127.0.0.2", 5002, subprotocols=["custom-path"])
+start_server = websockets.serve(send_random_data, "127.0.0.2", 5001)
 
 asyncio.get_event_loop().run_until_complete(start_server)
-print("WebSocket server is running on ws://127.0.0.2:5002/custom-path")
+print("WebSocket server is running on ws://127.0.0.2:5001/custom-path")
 asyncio.get_event_loop().run_forever()
